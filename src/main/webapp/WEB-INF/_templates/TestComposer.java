38df5fda26e0a6c9749712823ae248eca31890f1
TonyQ Wang <tonylovejava@gmail.com>
2011-06-06 23:16:00 +0800

    add templates (fix gitignore) , remove loggers , add fb-like jar

---
+++ src/main/webapp/WEB-INF/_templates/TestComposer.java
@@ -0,0 +1,20 @@
+import org.zkoss.zk.ui.*;
+import org.zkoss.zk.ui.event.*;
+import org.zkoss.zk.ui.util.*;
+import org.zkoss.zul.*;
+
+public class TestComposer extends GenericForwardComposer{
+
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+	}
+	
+	public void onClick$btn(Event e){
+		try {
+			Messagebox.show("Hi btn");
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
