74919b8ffedf0b5e3f8050691f0b4e0b0ec16ec4
TonyQ Wang <tonylovejava@gmail.com>
2011-06-13 01:35:46 +0800

    move default resource to resource factory

--- src/main/java/org/zkoss/fiddle/composer/SourceCodeEditorComposer.java
+++ src/main/java/org/zkoss/fiddle/composer/SourceCodeEditorComposer.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
 import org.apache.log4j.Logger;
 import org.zkoss.fiddle.component.renderer.SourceTabRendererFactory;
 import org.zkoss.fiddle.composer.event.FiddleEventQueues;
@@ -34,7 +32,7 @@ import org.zkoss.fiddle.seo.SEOContainer;
 import org.zkoss.fiddle.seo.handle.SEOTokenHandlerAdpter;
 import org.zkoss.fiddle.seo.model.SEOToken;
 import org.zkoss.fiddle.util.CRCCaseIDEncoder;
-import org.zkoss.fiddle.util.FileUtil;
+import org.zkoss.fiddle.util.ResourceFactory;
 import org.zkoss.social.facebook.event.LikeEvent;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
@@ -229,7 +227,7 @@ public class SourceCodeEditorComposer extends GenericForwardComposer {
 
 				} else if (event instanceof SourceInsertEvent) {
 					SourceInsertEvent insertEvent = (SourceInsertEvent) event;
-					Resource ir = getDefaultResource(insertEvent.getType(), insertEvent.getFileName());
+					Resource ir = ResourceFactory.getDefaultResource(insertEvent.getType(), insertEvent.getFileName());
 					resources.add(ir);
 					SourceTabRendererFactory.getRenderer(ir.getType()).appendSourceTab(sourcetabs, sourcetabpanels, ir);
 					insertWin.setVisible(false);
@@ -349,76 +347,14 @@ public class SourceCodeEditorComposer extends GenericForwardComposer {
 		return newCase;
 	}
 
-	private String readThenReaplce(String filePath, String token, String replaced) {
-
-		ServletContext req = (ServletContext) Executions.getCurrent().getDesktop().getWebApp().getNativeContext();
-		String template = FileUtil.readIfExist(req.getRealPath(filePath));
-		if (token != null && template != null)
-			template = template.replaceAll(token, replaced);
-		return template;
-	}
-
-	/**
-	 * TODO move it to resource
-	 * 
-	 * @param type
-	 * @param name
-	 * @return
-	 */
-	private Resource getDefaultResource(int type, String name) {
-
-		if (IResource.TYPE_ZUL == type) {
-			String template = readThenReaplce("/WEB-INF/_templates/index.zul", "\\$\\{pkg\\}",
-					IResource.PACKAGE_TOKEN_ESCAPE);
-			return new Resource(IResource.TYPE_ZUL, name, template);
-		} else if (IResource.TYPE_JS == type) {
-			return new Resource(IResource.TYPE_JS, name, "function hello(){alert('hello');}");
-		} else if (IResource.TYPE_CSS == type) {
-			return new Resource(IResource.TYPE_CSS, name, ".hello{ \n color:red; \n }");
-		} else if (IResource.TYPE_HTML == type) {
-			return (new Resource(IResource.TYPE_HTML, name,
-					"<html>\n  <head>\n    <title>Hello</title>\n  </head>\n\n<body>\n    hello\n  </body>\n</html>"));
-		} else if (IResource.TYPE_JAVA == type) {
-			String clsName = name;
-			if (clsName != null)
-				clsName = name.replaceAll(".java", "");
-
-			String template = readThenReaplce("/WEB-INF/_templates/TestComposer.java", "\\$\\{class-name\\}", clsName);
-			return new Resource(IResource.TYPE_JAVA, name, template);
-		} else
-			return null;
-	}
-
-	/**
-	 * TODO move it to resource
-	 * 
-	 * @param type
-	 * @return
-	 */
-	private IResource getDefaultResource(int type) {
-		if (IResource.TYPE_ZUL == type) {
-			IResource r = getDefaultResource(type, "index.zul");
-			r.setCanDelete(false);
-			return r;
-		} else if (IResource.TYPE_JS == type)
-			return getDefaultResource(type, "index.js");
-		else if (IResource.TYPE_CSS == type)
-			return getDefaultResource(type, "index.css");
-		else if (IResource.TYPE_HTML == type)
-			return getDefaultResource(type, "index.html");
-		else if (IResource.TYPE_JAVA == type)
-			return getDefaultResource(type, "TestComposer.java");
-		else
-			return null;
-	}
 
 	private List<Resource> getDefaultResources() {
 		List resources = new ArrayList<IResource>();
-		resources.add(getDefaultResource(IResource.TYPE_ZUL));
+		resources.add(ResourceFactory.getDefaultResource(IResource.TYPE_ZUL));
 		// resources.add(getDefaultResource(IResource.TYPE_JS));
 		// resources.add(getDefaultResource(IResource.TYPE_CSS));
 		// resources.add(getDefaultResource(IResource.TYPE_HTML));
-		resources.add(getDefaultResource(IResource.TYPE_JAVA));
+		resources.add(ResourceFactory.getDefaultResource(IResource.TYPE_JAVA));
 
 		return resources;
 	}
---
+++ src/main/java/org/zkoss/fiddle/util/ResourceFactory.java
@@ -0,0 +1,62 @@
+package org.zkoss.fiddle.util;
+
+import javax.servlet.ServletContext;
+
+import org.zkoss.fiddle.model.Resource;
+import org.zkoss.fiddle.model.api.IResource;
+import org.zkoss.zk.ui.Executions;
+
+
+public class ResourceFactory {
+
+	public static Resource getDefaultResource(int type, String name) {
+
+		if (IResource.TYPE_ZUL == type) {
+			String template = readThenReaplce("/WEB-INF/_templates/index.zul", "\\$\\{pkg\\}",
+					IResource.PACKAGE_TOKEN_ESCAPE);
+			return new Resource(IResource.TYPE_ZUL, name, template);
+		} else if (IResource.TYPE_JS == type) {
+			return new Resource(IResource.TYPE_JS, name, "function hello(){alert('hello');}");
+		} else if (IResource.TYPE_CSS == type) {
+			return new Resource(IResource.TYPE_CSS, name, ".hello{ \n color:red; \n }");
+		} else if (IResource.TYPE_HTML == type) {
+			return (new Resource(IResource.TYPE_HTML, name,
+					"<html>\n  <head>\n    <title>Hello</title>\n  </head>\n\n<body>\n    hello\n  </body>\n</html>"));
+		} else if (IResource.TYPE_JAVA == type) {
+			String clsName = name;
+			if (clsName != null)
+				clsName = name.replaceAll(".java", "");
+
+			String template = readThenReaplce("/WEB-INF/_templates/TestComposer.java", "\\$\\{class-name\\}", clsName);
+			return new Resource(IResource.TYPE_JAVA, name, template);
+		} else
+			return null;
+	}
+
+	public static IResource getDefaultResource(int type) {
+		if (IResource.TYPE_ZUL == type) {
+			IResource r = getDefaultResource(type, "index.zul");
+			r.setCanDelete(false);
+			return r;
+		} else if (IResource.TYPE_JS == type)
+			return getDefaultResource(type, "index.js");
+		else if (IResource.TYPE_CSS == type)
+			return getDefaultResource(type, "index.css");
+		else if (IResource.TYPE_HTML == type)
+			return getDefaultResource(type, "index.html");
+		else if (IResource.TYPE_JAVA == type)
+			return getDefaultResource(type, "TestComposer.java");
+		else
+			return null;
+	}
+	
+	private static String readThenReaplce(String filePath, String token, String replaced) {
+
+		ServletContext req = (ServletContext) Executions.getCurrent().getDesktop().getWebApp().getNativeContext();
+		String template = FileUtil.readIfExist(req.getRealPath(filePath));
+		if (token != null && template != null)
+			template = template.replaceAll(token, replaced);
+		return template;
+	}
+
+}
