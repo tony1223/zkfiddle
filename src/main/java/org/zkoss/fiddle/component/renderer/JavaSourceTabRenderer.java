8bb28f95e9941257ea75c901828340403f0993ac
TonyQ Wang <tonylovejava@gmail.com>
2011-06-18 01:36:08 +0800

    #60 fine tune package input

--- src/main/java/org/zkoss/fiddle/component/renderer/JavaSourceTabRenderer.java
+++ src/main/java/org/zkoss/fiddle/component/renderer/JavaSourceTabRenderer.java
@@ -13,6 +13,7 @@ import org.zkoss.zk.ui.event.EventQueue;
 import org.zkoss.zk.ui.event.EventQueues;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.event.InputEvent;
+import org.zkoss.zul.A;
 import org.zkoss.zul.Constraint;
 import org.zkoss.zul.Hlayout;
 import org.zkoss.zul.Label;
@@ -32,38 +33,81 @@ public class JavaSourceTabRenderer extends SourceTabRenderer {
 		label.setSclass("c-like-keyword");
 		hlayout.appendChild(label);
 		 
-		Label label2 = new Label(IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN);
-		label2.setTooltiptext("since we have to prevent package conflict for every version ," + " so we use "
-				+ IResource.PACKAGE_TOKEN + " as your class package by default.");
-		hlayout.appendChild(label2);
 		
 		//we use desktop scope , so it's ok to lookup every time.
 		final EventQueue sourceQueue = EventQueues.lookup(FiddleEventQueues.SOURCE);
+
 		
-		Textbox txtPkg = new Textbox();
-		txtPkg.setInplace(true);
-		txtPkg.setValue(resource.getPkg());
-		txtPkg.setTooltiptext("(This is optional) you could set a package name "
-				+ " and it should start with a dot ,"
-				+ " but you don't have to modify the java filename , we will handle this.");
+		{
+			final Label label2 = new Label(IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN );
+			label2.setTooltiptext("since we have to prevent package conflict for every version ," + " so we use "
+					+ IResource.PACKAGE_TOKEN + " as your class package by default.");
+			hlayout.appendChild(label2);
 
-		txtPkg.setConstraint(new Constraint() {
-			public void validate(Component comp, Object value) throws WrongValueException {
-				if (!packageRule.matcher( IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN + (String) value).matches()) {
-					throw new WrongValueException("Not a valid package name");
+			label2.setVisible(false);
+			
+			final Textbox txtPkg = new Textbox();
+			txtPkg.setInplace(true);
+			txtPkg.setValue(resource.getPkg());
+			txtPkg.setTooltiptext("(This is optional) you could set a package name "
+					+ " and it should start with a dot ,"
+					+ " but you don't have to modify the java filename , we will handle this.");
+	
+			txtPkg.setConstraint(new Constraint() {
+				public void validate(Component comp, Object value) throws WrongValueException {
+					String fullval = IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN + (String) value;
+					boolean accepted =".".equals(value) || packageRule.matcher( fullval).matches();
+					
+					if (!accepted) {
+						throw new WrongValueException("Not a valid package name");
+					}
 				}
-			}
-		});
-		txtPkg.addEventListener(Events.ON_CHANGE, new EventListener() {
-			public void onEvent(Event event) throws Exception {
-				if(event instanceof InputEvent){
-					resource.setPkg(((InputEvent) event).getValue());
-					sourceQueue.publish(new SourceChangedEvent(null,resource));
+			});
+		
+			txtPkg.setVisible(false);
+			hlayout.appendChild(txtPkg);
+			
+			final A a = new A(IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN);
+			a.addEventListener(Events.ON_CLICK,new EventListener() {
+				public void onEvent(Event event) throws Exception {
+					
+					label2.setVisible(true);
+					txtPkg.setVisible(true);
+					a.setVisible(false);
+					if("".equals(txtPkg.getValue())){
+						txtPkg.setRawValue(".");
+					}
+					txtPkg.focus();
 				}
-			}
-		});
+			});
 
-		hlayout.appendChild(txtPkg);
+			txtPkg.addEventListener(Events.ON_BLUR, new EventListener() {
+				public void onEvent(Event event) throws Exception {
+					if(txtPkg.isValid()){
+						label2.setVisible(false);
+						txtPkg.setVisible(false);
+						a.setVisible(true);
+					}
+				}				
+			});
+			txtPkg.addEventListener(Events.ON_CHANGE, new EventListener() {
+				public void onEvent(Event event) throws Exception {
+					if(txtPkg.isValid()){
+						if(event instanceof InputEvent){
+							String value = ((InputEvent) event).getValue();
+							if(".".equals(value)){
+								value = "";
+							}
+							resource.setPkg(((InputEvent) event).getValue());
+							sourceQueue.publish(new SourceChangedEvent(null,resource));
+							a.setLabel(IResource.PACKAGE_PREFIX + IResource.PACKAGE_TOKEN + value);
+						}
+					}
+				}
+			});
+			
+			hlayout.appendChild(a);
+		}
 		
 		hlayout.appendChild(new Label(";"));
 		
