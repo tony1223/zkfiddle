14d9f4e45134ec3baf7c82148efc0a1abd9ea0f8
TonyQ Wang <tonylovejava@gmail.com>
2011-06-19 00:35:23 +0800

    #38 file builder bug fix

--- src/main/java/org/zkoss/fiddle/files/ResourceFile.java
+++ src/main/java/org/zkoss/fiddle/files/ResourceFile.java
@@ -9,19 +9,29 @@ public class ResourceFile implements IResourceFile {
 
 	private IResource resource;
 
+	private static String replacedSeparator = String.valueOf(File.separatorChar);
+	{
+		if("\\".equals(replacedSeparator)){
+			replacedSeparator = "\\\\";
+		}
+	}
+	
 	public ResourceFile(IResource ir) {
 		if (ir == null)
 			throw new IllegalArgumentException("resource can't be null");
 		resource = ir;
 	}
 
+	private String convertPackageToFolder(String inp){
+		if(inp == null) return "";
+		return inp.replaceAll(Pattern.quote("."),replacedSeparator);
+	}
+	
 	public String getPath() {
 		if (resource.getType() != IResource.TYPE_JAVA) {
 			return resource.getName();
 		} else {
-			String fullpackage = resource.getFullPackage().replaceAll(Pattern.quote("."),
-					Pattern.quote("" + File.separatorChar));
-			return "src/" + fullpackage + "/" + resource.getName();
+			return "src/" + convertPackageToFolder(resource.getFullPackage()) + "/" + resource.getName();
 		}
 
 	}
