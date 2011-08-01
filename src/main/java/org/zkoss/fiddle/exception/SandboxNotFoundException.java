1af3c074b7c5b94d89629a75c3001c8d7cc72616
Tony Wang <tonywang@potix.com>
2011-08-01 14:42:29 -0700

    fine tune the code and clean warning

--- src/main/java/org/zkoss/fiddle/exception/SandboxNotFoundException.java
+++ src/main/java/org/zkoss/fiddle/exception/SandboxNotFoundException.java
@@ -2,6 +2,11 @@ package org.zkoss.fiddle.exception;
 
 public class SandboxNotFoundException extends Exception {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 5813481233573259770L;
+
 	public enum Type {
 		HASH, ZK_VERSION,DEFAULT
 	}
