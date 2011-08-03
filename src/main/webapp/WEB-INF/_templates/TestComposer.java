d9de44e29839b1c77dc01df9111e7a0e67ecb729
Tony Wang <tonylovejava@gmail.com>
2011-08-03 16:32:22 -0700

    add AU package to template

--- src/main/webapp/WEB-INF/_templates/TestComposer.java
+++ src/main/webapp/WEB-INF/_templates/TestComposer.java
@@ -2,6 +2,8 @@ import org.zkoss.zk.ui.*;
 import org.zkoss.zk.ui.event.*;
 import org.zkoss.zk.ui.util.*;
 import org.zkoss.zk.ui.ext.*;
+import org.zkoss.zk.au.*;
+import org.zkoss.zk.au.out.*;
 import org.zkoss.zul.*;
 
 public class ${class-name} extends GenericForwardComposer{
