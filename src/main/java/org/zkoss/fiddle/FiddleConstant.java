1f2453c3199e8c3721f815c7762ffe3d48b52cd3
Tony Wang <tonylovejava@gmail.com>
2011-08-17 16:07:44 -0700

    add the missing final keyword

--- src/main/java/org/zkoss/fiddle/FiddleConstant.java
+++ src/main/java/org/zkoss/fiddle/FiddleConstant.java
@@ -2,20 +2,19 @@ package org.zkoss.fiddle;
 
 
 public class FiddleConstant {
-	public static String REQUEST_ATTR_CASE = "__case";
+	public static final String REQUEST_ATTR_CASE = "__case";
 
-	public static String REQUEST_ATTR_RUN_VIEW = "runview";
+	public static final String REQUEST_ATTR_RUN_VIEW = "runview";
 
-	public static String REQUEST_ATTR_PAGE_TITLE = "_pgtitle";
+	public static final String REQUEST_ATTR_PAGE_TITLE = "_pgtitle";
 
-	public static String REQUEST_ATTR_TRY_CASE = "_trycase";
-	
-	public static String DESKTOP_ATTR_PUSH_STATE = "d_pushstate";
-	
-	public static String DESKTOP_ATTR_CURRENT_STATE_URI = "d_state_uri";
-	
-	
-	public static String AU_PUSH_STATE_INIT = "onPushStateInit";
-	
-	public static String AU_PUSH_STATE_CHANGE = "onStateChange";
+	public static final String REQUEST_ATTR_TRY_CASE = "_trycase";
+
+	public static final String DESKTOP_ATTR_PUSH_STATE = "d_pushstate";
+
+	public static final String DESKTOP_ATTR_CURRENT_STATE_URI = "d_state_uri";
+
+	public static final String AU_PUSH_STATE_INIT = "onPushStateInit";
+
+	public static final String AU_PUSH_STATE_CHANGE = "onStateChange";
 }
