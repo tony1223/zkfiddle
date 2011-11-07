919d8db62050fb7c82b1809f14bfa84e6e8b5a0b
TonyQ Wang <tonylovejava@gmail.com>
2011-11-08 05:44:56 +0800

    change implementation for multiple sandbox

--- src/main/java/org/zkoss/fiddle/filter/FiddleSandboxFilter.java
+++ src/main/java/org/zkoss/fiddle/filter/FiddleSandboxFilter.java
@@ -11,29 +11,82 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.zkoss.fiddle.manager.FiddleSandboxManager;
 import org.zkoss.fiddle.visualmodel.FiddleSandbox;
 
 public class FiddleSandboxFilter implements Filter {
+
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(FiddleSandboxFilter.class);
+
 	private FiddleSandboxManager sandboxManager;
+
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
 			ServletException {
 		HttpServletRequest httprequest = ((HttpServletRequest) request);
 
-		FiddleSandbox in = new FiddleSandbox();
-		in.setName(httprequest.getParameter("name"));
-		in.setPath(httprequest.getParameter("path"));
-		in.setVersion(httprequest.getParameter("ver"));
-		in.setLastUpdate(new Date());
+		int size = -1;
+		try {
+			String sizePar = httprequest.getParameter("size");
+			size = Integer.parseInt(sizePar);
+		} catch (Exception ex) {
+			logger.warn("size is valid number.", ex);
+		}
+
 		
+		
+		
+		if(size !=-1){
+			boolean success = true;
+			
+			for (int i = 0; i < size; ++i) {
+				boolean curSuccess = addSandbox(httprequest.getParameter("name"+i), 
+						httprequest.getParameter("path"+i),
+						httprequest.getParameter("ver"+i)) ;
+				success = success && curSuccess;
+				
+			}
+			
+			if (success) {
+				response.getWriter().println("true");
+			} else {
+				response.getWriter().println("false");
+			}
+
+			response.getWriter().close();
+		}else {
+			//2011/11/8 Tony: Currently nobody is using this way , plan to remove this in future.
+			boolean success = addSandbox(httprequest.getParameter("name"), httprequest.getParameter("path"),
+					httprequest.getParameter("ver"));
+
+			if (success) {
+				response.getWriter().println("true");
+			} else {
+				response.getWriter().println("false");
+			}
+
+			response.getWriter().close();
+
+		}
+	}
+
+	private boolean addSandbox(String name, String path, String ver) {
+		FiddleSandbox in = new FiddleSandbox();
+		in.setName(name);
+		in.setPath(path);
+		in.setVersion(ver);
+		in.setLastUpdate(new Date());
+
 		try {
 			sandboxManager.addFiddleSandbox(in);
-			response.getWriter().println("true");
+			return true;
 		} catch (Exception e) {
-			e.printStackTrace();
-			response.getWriter().println("false");
+			logger.error("Exception happened when add sandbox to the queue", e);
+			return false;
 		}
-		response.getWriter().close();
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -42,11 +95,10 @@ public class FiddleSandboxFilter implements Filter {
 	public void destroy() {
 	}
 
-	
 	public FiddleSandboxManager getSandboxManager() {
 		return sandboxManager;
 	}
-	
+
 	public void setSandboxManager(FiddleSandboxManager sandboxManager) {
 		this.sandboxManager = sandboxManager;
 	}
